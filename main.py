7da54a24b9df36cba9cb9efafd9d9054dd5f3a5c
Abdur Rahman <abdur.r10567@gmail.com>
2022-11-25 21:09:23 -0800

    Added all the menus and finished part 1

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sqlite3
+from consolemenu import *
+from consolemenu.items import *
 
-databaseConnection = sqlite3.connect('test.db')
+databaseConnection = sqlite3.connect('test.db', check_same_thread=False)
 cur = databaseConnection.cursor()
 print("Opened database successfully")
 
+
 def initializeDatabase():
-    #Add all tables to database
+    # Add all tables to database
     cur.execute("CREATE TABLE IF NOT EXISTS Trip ("
                 "TripNumber integer NOT NULL,"
                 "StartLocationName text,"
@@ -57,7 +60,7 @@ def initializeDatabase():
                 "NumberOfPassengerOut integer,"
                 "PRIMARY KEY (TripNumber, Date, ScheduledStartTime, StopNumber),"
                 "FOREIGN KEY (TripNumber) REFERENCES Trip (TripNumber),"
-                "FOREIGN KEY (Date) REFERENCES TripOffering (Date),"
+                #"FOREIGN KEY (Date) REFERENCES TripOffering (Date),"
                 "FOREIGN KEY (ScheduledStartTime) REFERENCES TripOffering (ScheduledStartTime),"
                 "FOREIGN KEY (StopNumber) REFERENCES Stop (StopNumber),"
                 "FOREIGN KEY (ScheduledArrivalTime) REFERENCES TripOffering (ScheduledArrivalTime)"
@@ -73,14 +76,126 @@ def initializeDatabase():
     databaseConnection.commit()
 
 
+def displaySchedule():
+    startLocationName = input("Enter a start location: ")
+    startLocationName.lower()
+    DestinationName = input("Enter a destination: ")
+    DestinationName.lower()
+    Date = input("Enter a date in YYYY-MM-DD format: ")
+    SQL_Query = "SELECT T2.* " \
+                "FROM TripOffering T2 " \
+                "WHERE T2.Date =\'" + Date + "\' AND T2.TripNumber IN(SELECT T.TripNumber " \
+                                             "FROM Trip T " \
+                                             "WHERE T.StartLocationName =\'" + startLocationName + "\' " \
+                                                                                                   "AND T.DestinationName=\'" + DestinationName + "\');"
+    cur.execute(SQL_Query)
+    output = cur.fetchall()
+    for row in output:
+        print(row)
 
+
+def deleteTripOffering():
+    TripNumber = input("Enter a Trip Number: ")
+    Date = input("Enter a date in YYYY-MM-DD format: ")
+    ScheduledStartTime = input("Enter the scheduled start time: ")
+    ScheduledStartTime.lower()
+    SQL_Query = "SELECT T2.* " \
+                "FROM TripOffering T2 " \
+                "WHERE T2.Date =\'" + Date + "\' AND T2.TripNumber IN(SELECT T.TripNumber " \
+                                             "FROM Trip T " \
+                                             "WHERE T.StartLocationName =\'" + startLocationName + "\' " \
+                                                                                                   "AND T.DestinationName=\'" + DestinationName + "\');"
+    cur.execute(SQL_Query)
+
+
+def addTripOffering():
+    print("option 2 lol")
+
+
+def changeDriver():
+    print("option 3 lol")
+
+
+def changeBus():
+    print("option 4 lol")
+
+
+def displayStops():
+    print("Display stops")
+
+
+def displayDriverSchedule():
+    print("Display Driver schedule")
+
+def addDriver():
+    print("Add a driver")
+
+def addBus():
+    print("Add a Bus")
+
+def deleteBus():
+    print("Delete a bus")
+
+def insertActualTrip():
+    print("Insert Actual Trip")
 
 def main():
-    # create some hard coded processes.
+    # initialize our database and create its tables
     initializeDatabase()
     print("Database initialized Successfully")
 
+    # create a menu with options
+    menu = ConsoleMenu("Main Menu")
+    function_item1 = FunctionItem(
+        "Display the schedule of all trips for a given start location, destination, and date.", displaySchedule)
 
+    function_item2 = FunctionItem(
+        "Display the stops of a given trip.", displayStops)
+
+    function_item3 = FunctionItem(
+        "Display the weekly schedule of a given driver and date.", displayDriverSchedule)
+
+    function_item4 = FunctionItem(
+        "Add a driver.", addDriver)
+
+    function_item5 = FunctionItem(
+        "Add a bus.", addBus)
+
+    function_item6 = FunctionItem(
+        "Delete a bus.", deleteBus)
+
+    function_item7 = FunctionItem(
+        "Insert data for an actual trip specified by its key.", insertActualTrip)
+
+    editScheduleSubmenu = ConsoleMenu("Edit Schedules")
+    editScheduleOption1 = FunctionItem("Delete a trip offering by TripNumber, Date, and Scheduled Start Time.",
+                                       deleteTripOffering)
+
+    # WHAT DOES THE PROFESSOR WANT FOR THIS ONE?
+    editScheduleOption2 = FunctionItem(
+        "Add a set of trip offerings assuming the values of all attributes are given (the software"
+        " asks if you have more trips to enter)", addTripOffering)
+
+    editScheduleOption3 = FunctionItem("Change the driver for a given Trip Offering.", changeDriver)
+    editScheduleOption4 = FunctionItem("Change the bus for a given Trip Offering.", changeBus)
+
+    editScheduleSubmenu.append_item(editScheduleOption1)
+    editScheduleSubmenu.append_item(editScheduleOption2)
+    editScheduleSubmenu.append_item(editScheduleOption3)
+    editScheduleSubmenu.append_item(editScheduleOption4)
+
+    submenu_item_1 = SubmenuItem("Edit Schedules", submenu=editScheduleSubmenu)
+    submenu_item_1.set_menu(menu)
+
+    menu.append_item(function_item1)
+    menu.append_item(submenu_item_1)
+    menu.append_item(function_item2)
+    menu.append_item(function_item3)
+    menu.append_item(function_item4)
+    menu.append_item(function_item5)
+    menu.append_item(function_item6)
+    menu.append_item(function_item7)
+    menu.show()
 
 
 # Press the green button in the gutter to run the script.
