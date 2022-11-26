cb4d9d17373a391c525e7e07c233acac30d398d1
Abdur Rahman <abdur.r10567@gmail.com>
2022-11-25 18:05:09 -0800

    Added SQL commands for creating tables

--- main.py
+++ main.py
@@ -1,16 +1,90 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sqlite3
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+databaseConnection = sqlite3.connect('test.db')
+cur = databaseConnection.cursor()
+print("Opened database successfully")
+
+def initializeDatabase():
+    #Add all tables to database
+    cur.execute("CREATE TABLE IF NOT EXISTS Trip ("
+                "TripNumber integer NOT NULL,"
+                "StartLocationName text,"
+                "DestinationName text,"
+                "PRIMARY KEY (TripNumber)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS TripOffering ("
+                "TripNumber integer NOT NULL,"
+                "Date text NOT NULL,"
+                "ScheduledStartTime text NOT NULL,"
+                "ScheduledArrivalTime text,"
+                "DriverName text NOT NULL,"
+                "BusID integer NOT NULL,"
+                "PRIMARY KEY (TripNumber, Date, ScheduledStartTime),"
+                "FOREIGN KEY (TripNumber) REFERENCES Trip (TripNumber),"
+                "FOREIGN KEY (BusID) REFERENCES Bus (BusID),"
+                "FOREIGN KEY (DriverName) REFERENCES Driver (DriverName)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS Bus ("
+                "BusID integer NOT NULL,"
+                "Model text,"
+                "Year text,"
+                "PRIMARY KEY (BusID)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS Driver ("
+                "DriverName text NOT NULL,"
+                "DriverTelephoneNumber integer,"
+                "PRIMARY KEY (DriverName)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS Stop ("
+                "StopNumber integer NOT NULL,"
+                "StopAddress text,"
+                "PRIMARY KEY (StopNumber)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS ActualTripStopInfo ("
+                "TripNumber integer NOT NULL,"
+                "Date text NOT NULL,"
+                "ScheduledStartTime text NOT NULL,"
+                "StopNumber integer NOT NULL,"
+                "ScheduledArrivalTime text,"
+                "ActualStartTime text,"
+                "ActualArrivalTime text,"
+                "NumberOfPassengerIn integer,"
+                "NumberOfPassengerOut integer,"
+                "PRIMARY KEY (TripNumber, Date, ScheduledStartTime, StopNumber),"
+                "FOREIGN KEY (TripNumber) REFERENCES Trip (TripNumber),"
+                "FOREIGN KEY (Date) REFERENCES TripOffering (Date),"
+                "FOREIGN KEY (ScheduledStartTime) REFERENCES TripOffering (ScheduledStartTime),"
+                "FOREIGN KEY (StopNumber) REFERENCES Stop (StopNumber),"
+                "FOREIGN KEY (ScheduledArrivalTime) REFERENCES TripOffering (ScheduledArrivalTime)"
+                ");")
+    databaseConnection.commit()
+    cur.execute("CREATE TABLE IF NOT EXISTS TripStopInfo ("
+                "TripNumber integer NOT NULL,"
+                "StopNumber integer NOT NULL,"
+                "SequenceNumber integer,"
+                "DrivingTime text,"
+                "PRIMARY KEY (TripNumber, StopNumber)"
+                ");")
+    databaseConnection.commit()
+
+
+
+
+def main():
+    # create some hard coded processes.
+    initializeDatabase()
+    print("Database initialized Successfully")
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
